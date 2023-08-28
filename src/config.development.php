2de0056c2ebd1a14a16a28c4f1f0b8a81b84215b
Jack Otieno <jack.otieno@ymail.com>
2023-08-28 23:28:50 +0300

    Added some inpatient functionality

--- src/config.development.php
+++ src/config.development.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "01fN741bT643ar9I", // API Secret Key
+        "SECRET_KEY" => "5LXlRISO7mf6Kb6i", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
--- src/config.php
+++ src/config.php
@@ -142,7 +142,7 @@ $CONFIG = [
 
     // General
     "UNFORMAT_YEAR" => 50, // Unformat year
-    "RANDOM_KEY" => '2Dbp1lsb9SHaTI8f', // Random key for encryption
+    "RANDOM_KEY" => 'hM1UPqrY8rAT6ZxJ', // Random key for encryption
     "ENCRYPTION_KEY" => '', // Encryption key for data protection
     "PROJECT_STYLESHEET_FILENAME" => "css/jootidigitalhealthcare.css", // Project stylesheet file name
     "USE_COMPRESSED_STYLESHEET" => true, // Compressed stylesheet
