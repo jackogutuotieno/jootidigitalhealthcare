4f83c7cf79c665f8d61c1e55fab7008dea80bb64
Jack Otieno <jack.otieno@ymail.com>
2023-10-23 10:26:50 +0300

    Updated log file

--- src/config.development.php
+++ src/config.development.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "theclinicianshub_1044", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "zyf3jz4cW1s0JSk9", // API Secret Key
+        "SECRET_KEY" => "4Q8n5C8Pk7wEIXmt", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
--- src/config.production.php
+++ src/config.production.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "theclinicianshub_1044", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "1b4i3ejk6zEHbwpB", // API Secret Key
+        "SECRET_KEY" => "P79nMxyh2MedyciG", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
